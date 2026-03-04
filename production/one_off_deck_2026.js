3cfa816e9f07b022904a4d4c361b9d99431a1490
Oliver <oliverhartley@google.com>
2026-03-04 17:18:37 -0300

    feat(2026): Collapse Solution column groups by default in Deep Dive

--- production/one_off_deck_2026.js
+++ production/one_off_deck_2026.js
@@ -296,6 +296,8 @@ function formatTestDeepDivePivot(sheet, lastRow, lastCol, rawDataStartRow) {
 
         // Add Native Column Grouping
         sheet.getRange(1, currentCol, 1, numProducts).shiftColumnGroupDepth(1);
+        const group = sheet.getColumnGroup(currentCol, 1);
+        if (group) group.collapse();
 
         currentCol += numProducts;
       }
